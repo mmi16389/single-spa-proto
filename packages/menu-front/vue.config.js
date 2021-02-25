b4a9531adf6aa54f2b8a056ffbe9caa26cb7acb1
mmi16389 <yvon.michel.ext@capgemini.com>
2021-02-25 22:20:06 +0100

    fix allow acces origin

--- packages/menu-front/vue.config.js
+++ packages/menu-front/vue.config.js
@@ -1,54 +1,57 @@
-const path = require('path');
-process.env.VUE_APP_VERSION = require('./package.json').version;
-const SystemJSPublicPathWebpackPlugin = require('systemjs-webpack-interop/SystemJSPublicPathWebpackPlugin');
-const { vueDotLoader } = require('@cnamts/vue-cli-plugin-vue-dash/vueDotLoader');
+const path = require("path");
+process.env.VUE_APP_VERSION = require("./package.json").version;
+const SystemJSPublicPathWebpackPlugin = require("systemjs-webpack-interop/SystemJSPublicPathWebpackPlugin");
+const {
+	vueDotLoader,
+} = require("@cnamts/vue-cli-plugin-vue-dash/vueDotLoader");
 
 module.exports = {
 	devServer: {
-		writeToDisk: true
+		writeToDisk: true,
 	},
 	configureWebpack: {
 		entry: {
-			remoteEntry: './src/main.ts'
+			remoteEntry: "./src/main.ts",
 		},
 		output: {
-			filename: '[name].js',
-			libraryTarget: 'umd',
-			path: path.resolve(__dirname, 'dist')
-		}
+			filename: "[name].js",
+			libraryTarget: "umd",
+			path: path.resolve(__dirname, "dist"),
+		},
+		headers: {
+			"Access-Control-Allow-Origin": "*",
+		},
 	},
 	chainWebpack: (config) => {
-		config.plugin('VuetifyLoaderPlugin').tap(vueDotLoader);
-		config.entryPoints.delete('app');
-		config.entry('remoteEntry')
-			.add('./src/main.ts')
-			.end();
+		config.plugin("VuetifyLoaderPlugin").tap(vueDotLoader);
+		config.entryPoints.delete("app");
+		config.entry("remoteEntry").add("./src/main.ts").end();
 		config
-			.plugin('SystemJSPublicPathWebpackPlugin')
+			.plugin("SystemJSPublicPathWebpackPlugin")
 			.use(SystemJSPublicPathWebpackPlugin, [
 				{
 					rootDirectoryLevel: 1,
-					systemjsModuleName: require('./package.json').name
-				}
+					systemjsModuleName: require("./package.json").name,
+				},
 			]);
-		if (process.env.STANDALONE_SINGLE_SPA !== 'true') {
+		if (process.env.STANDALONE_SINGLE_SPA !== "true") {
 			config.externals([
-				'vue',
-				'vue-router',
-				'vue-i18n',
-				'single-spa',
-				'satisfaction-cli-front',
-				'accueil-front',
-				'@cnamts/vue-dot'
+				"vue",
+				"vue-router",
+				"vue-i18n",
+				"single-spa",
+				"satisfaction-cli-front",
+				"accueil-front",
+				"@cnamts/vue-dot",
 			]);
 		} else {
 			config.externals([
-				'satisfaction-cli-front',
-				'accueil-front',
-				'single-spa',
-				'@cnamts/vue-dot'
+				"satisfaction-cli-front",
+				"accueil-front",
+				"single-spa",
+				"@cnamts/vue-dot",
 			]);
 		}
 	},
-	filenameHashing: false
+	filenameHashing: false,
 };
